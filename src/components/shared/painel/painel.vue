17308d96b5c46dd6508c25431e92b61b972e1d69
Moises Pompilio <moisesjtmed@gmail.com>
2022-02-08 10:29:20 -0300

    primeiro commit

---
+++ src/components/shared/painel/painel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="painel">
+    <h2 class="painel-titulo" @dblclick="visilve = !visilve">{{ titulo }}</h2>
+    <transition name="painel-fade">
+      <div class="painel-conteudo" v-show="visilve">
+        <slot> </slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["titulo"],
+  data() {
+    return {
+      visilve: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: rgb(191, 193, 194);
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+* {
+  box-shadow: 5px 5px 10px black;
+}
+.painel-fade-enter, .painel-fade-leave-active {
+  opacity: 0
+}
+
+.painel-fade-enter-active, .painel-fade-leave-active {
+  transition: opacity .5s
+}
+</style>
