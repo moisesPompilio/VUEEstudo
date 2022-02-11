5f5e2b7082d213cce64043cfe868538c551b2759
Moises Pompilio <moisesjtmed@gmail.com>
2022-02-11 12:12:12 -0300

    primeiro modolo finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,28 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-    <input class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre pelo título da foto">
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" v-bind:key="foto.titulo" >
-         <meu-painel :titulo="foto.titulo">
-           <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
-         </meu-painel>
-      </li>
-    </ul>
+    <meu-menu :rotas = "routes"/>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/painel.vue';
-import imagemResponsiva from './components/shared/imagem-responsiva/imagemResponsiva.vue';
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue';
 export default {
   components: {
-    'meu-painel' : Painel,
-    'imagem-responsiva' : imagemResponsiva,
+    "meu-menu" : Menu
   },
-  data() {
-    return {
-      titulo: "alurapic",
-      fotos: [],
-      filtro: ''
-    }
-  },
-  computed: {
-    fotosComFiltro(){
-      if(this.filtro){
-        let exp = new RegExp(this.filtro.trim(),'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      }else{
-        return this.fotos;
-      }
-    }
-  },
-    created() {
 
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+  data(){
+
+    return{
+      routes
+    }
   }
+
 }
 </script>
 
@@ -52,19 +32,13 @@ export default {
   width: 96%;
   margin: 0 auto;
 }
-.centralizado{
-  text-align: center;
-}
-.lista-fotos{
-  list-style: none;
-}
-.lista-fotos .lista-fotos-item{
-  display: inline-block;
+
+.pagina-enter, .pagina-leave-active {
+  opacity: 0
 }
 
-.filtro{
-  display: block;
-  width: 100%;
+.pagina-enter-active, .pagina-leave-active {
+  transition: opacity .5s
 }
 
 </style>
